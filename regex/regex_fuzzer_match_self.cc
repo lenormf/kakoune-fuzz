1417d3a8c28eaea1bf4092f5b0cfb2d070d4544d
Frank LENORMAND <lenormf@gmail.com>
2017-10-06 15:52:19 +0300

    regex: Add a fuzzer for the `regex_match()` function

---
+++ regex/regex_fuzzer_match_self.cc
@@ -0,0 +1,21 @@
+#include <string.hh>
+#include <regex_impl.hh>
+#include <exception.hh>
+
+using namespace Kakoune;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    String data_str{(const char*)data, (ByteCount)size};
+
+    try
+    {
+        Vector<String::iterator> matches;
+        CompiledRegex re = compile_regex(data_str);
+        regex_match(data_str.begin(), data_str.end(), matches, re);
+    }
+    catch (runtime_error&) {}
+    catch (utf8::invalid_codepoint&) {}
+
+    return 0;
+}
