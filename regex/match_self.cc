70220d3350027b772cac86ed2230b0fc86fff7cc
Frank LENORMAND <lenormf@gmail.com>
2017-10-10 15:29:51 +0300

    regex: Adapt to upstream API changes

--- regex/match_self.cc
+++ regex/match_self.cc
@@ -11,7 +11,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     try
     {
         Vector<String::iterator> matches;
-        CompiledRegex re = compile_regex(data_str);
+        CompiledRegex re = compile_regex(data_str, RegexCompileFlags::None);
         regex_match(data_str.begin(), data_str.end(), matches, re);
     }
     catch (runtime_error&) {}
